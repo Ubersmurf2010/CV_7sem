c8b127f83180fde5cee29833d0ef6afb72eb8939
Uubersmurf <georgiy.kazantsev.91@mail.ru>
2023-12-09 17:52:06 +0300

    lb2 update

--- lb2/main.cpp
+++ lb2/main.cpp
@@ -1,6 +1,52 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
 
+
+// Timers's class
+class CV_EXPORTS TickMeter
+{
+public:
+  TickMeter();
+  void start();
+  void stop();
+
+  int64 getTimeTicks() const;
+  double getTimeMicro() const;
+  double getTimeMilli() const;
+  double getTimeSec()   const;
+  int64 getCounter() const;
+
+  void reset();
+private:
+  int64 counter;
+  int64 sumTime;
+  int64 startTime;
+};
+
+std::ostream& operator << (std::ostream& out, const TickMeter& tm);
+
+TickMeter::TickMeter() { reset(); }
+int64 TickMeter::getTimeTicks() const { return sumTime; }
+double TickMeter::getTimeMicro() const { return  getTimeMilli() * 1e3; }
+double TickMeter::getTimeMilli() const { return getTimeSec() * 1e3; }
+double TickMeter::getTimeSec() const { return (double)getTimeTicks() / cv::getTickFrequency(); }
+int64 TickMeter::getCounter() const { return counter; }
+void TickMeter::reset() { startTime = 0; sumTime = 0; counter = 0; }
+
+void TickMeter::start() { startTime = cv::getTickCount(); }
+void TickMeter::stop()
+{
+  int64 time = cv::getTickCount();
+  if (startTime == 0)
+    return;
+  ++counter;
+  sumTime += (time - startTime);
+  startTime = 0;
+}
+
+std::ostream& operator << (std::ostream& out, const TickMeter& tm) { return out << tm.getTimeSec() << "sec"; }
+
+
 void box_filter(cv::Mat& src, cv::Mat& dst, int kernel_size) {
     assert(kernel_size % 2 == 1);
     dst.create(src.size(), src.type());
@@ -26,7 +72,7 @@ void compare(cv::Mat& img1, cv::Mat& img2, cv::Mat& result, double& interest) {
 
     for (int col = 0; col < img1.cols; col++) {
         for (int row = 0; row < img1.rows; row++) {
-            if (img1.at<uchar>(row, col) == img2.at<uchar>(row, col) || 
+            if (img1.at<uchar>(row, col) == img2.at<uchar>(row, col) ||
             img1.at<uchar>(row, col) == img2.at<uchar>(row, col) + 1 ||
             img1.at<uchar>(row, col) == img2.at<uchar>(row, col) - 1)
              {
@@ -52,11 +98,11 @@ void laplasFiltration(cv::Mat& src, cv::Mat& dst) {
     dst = src.clone();
     for (int col = 1; col < src.cols - 1; col++)
         for(int row = 1; row < src.rows - 1; row++) {
-            dst.at<uchar>(row, col) = std::round((src.at<uchar>(row, col) * (-4) + 
-            src.at<uchar>(row + 1, col) + 
-            src.at<uchar>(row - 1, col) + 
-            src.at<uchar>(row, col + 1) + 
-            src.at<uchar>(row, col - 1)) / 9); 
+            dst.at<uchar>(row, col) = std::round((src.at<uchar>(row, col) * (4) + 
+            (-1) * src.at<uchar>(row + 1, col) + 
+            (-1) * src.at<uchar>(row - 1, col) + 
+            (-1) * src.at<uchar>(row, col + 1) + 
+            (-1) * src.at<uchar>(row, col - 1)) / 9); 
         }
 }
 
@@ -68,7 +114,10 @@ void logTransform(cv::Mat& img1, cv::Mat& img2, cv::Mat& result, double c = 0.5)
     }
 }
 
-int main() {
+int main(int argc, char** argv) {
+    // set timer
+    TickMeter timer;
+
     cv::Mat img = cv::imread("/home/gerzeg/Polytech/TV/lb2/images/plank.jpg", cv::IMREAD_GRAYSCALE);
     if (img.empty()) {
         std::cout << "Could not read the image" << std::endl;
@@ -87,13 +136,21 @@ int main() {
 
     //box_filter
     cv::Mat box_filtered;
+    timer.start();
     box_filter(roi, box_filtered, 3);
+    timer.stop();
+    std::cout << "My time: " << timer.getTimeSec() << std::endl;
+    timer.reset();
     cv::imshow("box_filtered", box_filtered);
     cv::waitKey(-1);
 
     //opencv box_filter
     cv::Mat opencvFiltered;
+    timer.start();
     blur(roi, opencvFiltered, cv::Size(3, 3));
+    timer.stop();
+    std::cout << "Blur time: " << timer.getTimeSec() << std::endl;
+    timer.reset();
     cv::imshow("opencvFiltered", opencvFiltered);
     cv::waitKey(-1);
 
@@ -107,7 +164,7 @@ int main() {
     cv::Mat comparsion_1;
     double interest;
     compare(box_filtered, opencvFiltered, comparsion_1, interest);
-    std::cout << interest <<"%" << std::endl;
+    std::cout << interest <<"" << std::endl;
     cv::imshow("comparison BOX 1", comparsion_1);
     cv::waitKey(-1);
 
@@ -142,6 +199,12 @@ int main() {
     cv::Mat laplasSharp;
     unsharpMasking(roi, laplas, laplasSharp);
     cv::imshow("laplasSharp", laplasSharp);
+    cv::waitKey(-1);
+
+    // comparsion log between unsharp laplas and box
+    cv::Mat comparsion_4;
+    logTransform(boxSharp, laplasSharp, comparsion_4);
+    cv::imshow("comparison BOX 4", comparsion_4);
     cv::waitKey(-1);
     return 0;
 }
